a5987be9515489c96df1ecb1a63c0b4c1bf5cb42
Marcus Rognes <marcus.rognes@nte.no>
2023-11-13 11:22:11 +0100

    Setup basic api

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const url = require('url');
+
+http.createServer(function (req, res) {
+	const params = url.parse(req.url, true).query;
+	const ms = params.ms && parseInt(params.ms) || 4000;
+
+	setTimeout(() => {
+		res.write('OK');
+		res.end();
+	}, ms);
+}).listen(proces.env.PORT || 4000);
